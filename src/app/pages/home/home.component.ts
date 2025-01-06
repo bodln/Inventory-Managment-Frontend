fc8f871c9ddf6016db4e43a9ee05d808006a70a7
bodln <omer.sadikovic2001@gmail.com>
2025-01-06 18:00:47 +0100

    Add home page and navbar design changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,37 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   authService = inject(AuthService);
+
+  isAdmin(): boolean {
+    const userDetail = this.authService.getUserDetail();
+    return userDetail?.roles.includes('Admin');
+  }
+
+  isManager(): boolean {
+    const userDetail = this.authService.getUserDetail();
+    return userDetail?.roles.includes('Manager');
+  }
+
+  isWarehouseman(): boolean {
+    const userDetail = this.authService.getUserDetail();
+    return userDetail?.roles.includes('Warehouseman');
+  }
 }
