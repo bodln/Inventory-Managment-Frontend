d242b0362b8ebb31f02db777e125106b6955e7cd
bodln <omer.sadikovic2001@gmail.com>
2026-02-23 13:58:19 +0100

    change to cloudflare url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://178.221.242.236:8085/api'
+  apiUrl: 'https://wagner-acquisitions-font-people.trycloudflare.com/api'
   // apiUrl: 'https://evolved-gladly-possum.ngrok-free.app/api'
 };
