e8553192d4ac8e9e84324f3599819df35f6db033
bodln <omer.sadikovic2001@gmail.com>
2026-03-01 14:41:54 +0100

    Revert "Fix"
    This reverts commit 8cb6a25d72e77acca58f5e9129901899241b14a4.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     //apiUrl: 'https://evolved-gladly-possum.ngrok-free.app/api',
-    apiUrl: 'http://localhost:8084/api',
+    apiUrl: 'http://localhost:8083/api',
 };
