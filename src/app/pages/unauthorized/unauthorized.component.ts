667b6fcae3d136af6ba46345a21a5803dfb5d800
bodln <omer.sadikovic2001@gmail.com>
2026-03-01 14:22:07 +0100

    Make it more mobile firendly

--- src/app/pages/unauthorized/unauthorized.component.ts
+++ src/app/pages/unauthorized/unauthorized.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-unauthorized',
-    imports: [RouterLink],
+    imports: [RouterLink, MatIconModule],
     templateUrl: './unauthorized.component.html',
     styleUrl: './unauthorized.component.css'
 })
