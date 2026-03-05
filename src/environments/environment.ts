87b6018db333e5e3308c77f0b3f3e51abfd08274
bodln <omer.sadikovic2001@gmail.com>
2026-03-05 12:01:56 +0100

    api change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://earrings-relax-logical-banners.trycloudflare.com /api'
+  apiUrl: 'https://earrings-relax-logical-banners.trycloudflare.com/api'
   // apiUrl: 'https://evolved-gladly-possum.ngrok-free.app/api'
 };
